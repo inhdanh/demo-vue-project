81e507b1417efe6d37b514284dec58e5a2d65680
danh.dinh <danh.dinh@s3corp.com.vn>
2023-03-03 17:28:27 +0700

    login form

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,20 +1,30 @@
 <script setup>
-import { reactive } from 'vue';
+import { reactive, computed } from 'vue';
 
 const data = reactive({ username: '', password: '' })
+
 const login = () => {
-    console.log(data)
+    console.log(formValid.value)
+    if (formValid.value)
+        console.log(data)
 }
+const userNameValid = computed(() => data.username.trim().length > 0)
+const passwordValid = computed(() => data.password.trim().length > 0)
+const formValid = computed(() => userNameValid && passwordValid)
+
 </script>
 <template>
     <h1>Login</h1>
     <form @submit.prevent="login">
         <label>Username:
             <input v-model="data.username" type="text" />
+            <div v-if="!userNameValid">Please enter username</div>
         </label>
+        <br />
         <label>Password:
             <input v-model="data.password" type="password" />
+            <div v-if="!passwordValid">Please enter password</div>
         </label>
-        <button type="submit">Login</button>
+        <button type="submit" :disabled="!formValid">Login</button>
     </form>
 </template>
