a64e3074fd34da9e11b3e5b7eaace0e0b00b39f2
danh.dinh <danh.dinh@s3corp.com.vn>
2023-03-03 17:14:03 +0700

    Init commit

---
+++ src/views/LoginView.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { reactive } from 'vue';
+
+const data = reactive({ username: '', password: '' })
+const login = () => {
+    console.log(data)
+}
+</script>
+<template>
+    <h1>Login</h1>
+    <form @submit.prevent="login">
+        <label>Username:
+            <input v-model="data.username" type="text" />
+        </label>
+        <label>Password:
+            <input v-model="data.password" type="password" />
+        </label>
+        <button type="submit">Login</button>
+    </form>
+</template>
