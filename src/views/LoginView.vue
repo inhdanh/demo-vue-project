c37bf434e6c398ab7049b328e42b180742060b5b
danh.dinh <inh.danh@gmail.com>
2023-03-06 06:52:00 +0700

    login

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,30 +1,29 @@
 <script setup>
-import { reactive, computed } from 'vue';
+import { reactive, computed } from 'vue'
 
 const data = reactive({ username: '', password: '' })
 
 const login = () => {
-    console.log(formValid.value)
-    if (formValid.value)
-        console.log(data)
+  console.log(data)
 }
 const userNameValid = computed(() => data.username.trim().length > 0)
 const passwordValid = computed(() => data.password.trim().length > 0)
-const formValid = computed(() => userNameValid && passwordValid)
-
+const formValid = computed(() => userNameValid.value && passwordValid.value)
 </script>
 <template>
-    <h1>Login</h1>
-    <form @submit.prevent="login">
-        <label>Username:
-            <input v-model="data.username" type="text" />
-            <div v-if="!userNameValid">Please enter username</div>
-        </label>
-        <br />
-        <label>Password:
-            <input v-model="data.password" type="password" />
-            <div v-if="!passwordValid">Please enter password</div>
-        </label>
-        <button type="submit" :disabled="!formValid">Login</button>
-    </form>
+  <h1>Login</h1>
+  <form @submit.prevent="login">
+    <label
+      >Username:
+      <input v-model="data.username" type="text" />
+      <div v-if="!userNameValid">Please enter username</div>
+    </label>
+    <br />
+    <label
+      >Password:
+      <input v-model="data.password" type="password" />
+      <div v-if="!passwordValid">Please enter password</div>
+    </label>
+    <button type="submit" :disabled="!formValid">Login</button>
+  </form>
 </template>
